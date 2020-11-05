9a9441cdc13cb23be8943552cfc9b1b27475ba95
jmlinares2019 <jlinaresmar@gmail.com>
2020-11-05 18:54:51 +0100

    Sets up service ApiService

--- src/app/chuck/servicios/api.service.ts
+++ src/app/chuck/servicios/api.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Frase } from '../modelos/frase';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private frase: Frase = {
+    value: "",
+    icon_url: "",
+    id: "",
+    url: ""
+  };
+  private ChuckUrl = "https://api.chucknorris.io/jokes/random"; // URL to web api
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getFrase(): Observable<Frase>{
+    return this.http.get<Frase>(this.ChuckUrl);
+  }
+
 }
