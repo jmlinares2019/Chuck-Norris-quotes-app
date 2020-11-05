742347961d1a0371cd119894f4f694465ad9858e
jmlinares2019 <jlinaresmar@gmail.com>
2020-11-05 19:45:23 +0100

    Generates view component (only scaffolding)

---
+++ src/app/chuck/componentes/verfrase/verfrase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verfrase',
+  templateUrl: './verfrase.component.html',
+  styleUrls: ['./verfrase.component.css']
+})
+export class VerfraseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
