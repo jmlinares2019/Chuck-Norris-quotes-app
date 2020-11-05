9c8a7caed6279690f5bbe83e069ed83800219a5a
jmlinares2019 <jlinaresmar@gmail.com>
2020-11-05 18:11:56 +0100

    Creates 'chuck' module and imports it in main app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ChuckModule } from './chuck/chuck.module';
+
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ChuckModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
