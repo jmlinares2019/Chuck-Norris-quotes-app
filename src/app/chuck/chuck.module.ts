dafffb3ebe4cc5c9d9e3c763557b82dfe00068e1
jmlinares2019 <jlinaresmar@gmail.com>
2020-11-06 02:06:42 +0100

    Adds Material card style

--- src/app/chuck/chuck.module.ts
+++ src/app/chuck/chuck.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VerfraseComponent } from './componentes/verfrase/verfrase.component';
 
+// Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 @NgModule({
   declarations: [VerfraseComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    // Material
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
+  ],
   exports: [VerfraseComponent]
 })
 export class ChuckModule { }
